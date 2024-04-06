e399e40175e6176adcaa6e5a26ddaa39eeff8a8a
Dikshant Gupta <guptadikshant99@gmail.com>
2024-04-06 10:29:08 +0530

    first example of conversational chatbot using Streamlit Session State Variables

---
+++ Conversational_Chatbot/first_example.py
@@ -0,0 +1,55 @@
+from langchain_openai import OpenAI
+from langchain.memory.buffer import ConversationBufferMemory
+from langchain.prompts import PromptTemplate
+from langchain.chains.llm import LLMChain
+import streamlit as st
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+
+# intialize the LLM Model
+llm_model = OpenAI(
+    model="gpt-3.5-turbo-instruct",
+    temperature=0.6,
+    verbose=True
+)
+
+conv_mem = ConversationBufferMemory()
+
+chat_prompt = PromptTemplate(
+    input_variables=["history", "input"],
+    template="""
+You are a conversational Chatbot which helps to resolve people queries.
+conversation history: {history}
+human: {input}
+AI: 
+"""
+)
+
+# intialize the streamlit app
+st.title("Simple ChatBot Demo")
+input_text = st.text_input("Please Enter Your Queries.......")
+
+# Session State Variable
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
+else:
+    for message in st.session_state.chat_history:
+        conv_mem.save_context({"input": message["human"]}, {"output": message["AI"]})
+
+
+conv_chain = LLMChain(
+    llm=llm_model,
+    prompt=chat_prompt,
+    memory=conv_mem,
+    verbose=True
+)
+
+
+if input_text:
+    llm_response = conv_chain(input_text)["text"]
+    message = {'human': input_text, 'AI': llm_response}
+    st.session_state.chat_history.append(message)
+    st.write(llm_response)
+
+
