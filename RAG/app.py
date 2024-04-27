7382b9d52c0a686312036f30f958ba859df3f83c
Dikshant Gupta <guptadikshant99@gmail.com>
2024-04-27 23:05:30 +0530

    basic app created

--- RAG/app.py
+++ RAG/app.py
@@ -1,26 +1,41 @@
 import os
-import zipfile
+
 import streamlit as st
+from dotenv import find_dotenv, load_dotenv
+from utils import extract_zip, rag_pipeline
 
+load_dotenv(find_dotenv())
 
-def extract_zip(zip_file_path, extract_path):
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_path)
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY", "")
 
+def get_extracted_file_path():
+    pass
 
 def main():
+    extract_dir = "extracted_files"
     st.title("RAG Based Research QA Bot")
     uploaded_file = st.sidebar.file_uploader("Upload Research papers zip file", type="zip")
-
     submitted = st.sidebar.button(label="upload")
+    
+    
 
     if uploaded_file and submitted:
         st.sidebar.write("File uploaded")
-        extract_dir = "extracted_files"
         os.makedirs(extract_dir, exist_ok=True)
-        
-
         extract_zip(uploaded_file, extract_dir)
+        print(">>>>>> zip file extracted <<<<<<")
+        chain = rag_pipeline(os.path.abspath(extract_dir))
+        print(">>>>>> qa chain created <<<<<<")
+        input_text = st.chat_input(placeholder="Please write your question here.")
+        if input_text:
+            if chain:
+                print(chain)
+                print(f"inside input text: {input_text}")
+                llm_resp = chain.invoke({"query": input_text})
+                st.write(llm_resp["result"])
+            else:
+                st.write("Please upload a research paper zip file to create the QA chain.")
+
 
 
 if __name__ == "__main__":
--- RAG/utils.py
+++ RAG/utils.py
@@ -1,3 +1,65 @@
-from langchain_community.document_loaders.directory import DirectoryLoader
-from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader, PyPDFLoader
-from langchain.embeddings.openai import OpenAIEmbeddings
+import os
+import zipfile
+
+from langchain.chains import RetrievalQA
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.chroma import Chroma
+from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_openai import OpenAI, OpenAIEmbeddings
+
+
+def extract_zip(zip_file_path, extract_path):
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+
+
+def read_pdfs(dir_path):
+    loader = PyPDFDirectoryLoader(dir_path)
+    docs = loader.load()
+    return docs
+
+
+def convert_docs_into_chunks(documents):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splitted_docs = text_splitter.split_documents(documents)
+    return splitted_docs
+
+def create_embeddings_and_store_vectordb(texts):
+    persist_dir = "db"
+
+    openai_embedding = OpenAIEmbeddings()
+
+    vectordb = Chroma.from_documents(
+        documents=texts,
+        embedding=openai_embedding,
+        persist_directory=persist_dir
+    )
+
+    vectordb.persist()
+
+    vectordb = Chroma(
+    persist_directory=os.path.abspath(persist_dir), 
+    embedding_function=openai_embedding
+)
+    retriever = vectordb.as_retriever()
+
+    return retriever
+
+def create_qa_chain(retriever):
+    
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=OpenAI(),
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True
+)
+    return qa_chain
+    
+def rag_pipeline(dir_path):
+    docs = read_pdfs(dir_path)
+    splitted_docs = convert_docs_into_chunks(docs)
+    retriver = create_embeddings_and_store_vectordb(splitted_docs)
+    rag_qa_chain = create_qa_chain(retriver)
+
+    return rag_qa_chain
+
